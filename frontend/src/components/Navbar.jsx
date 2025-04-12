151a7b8f1130d5e7de5b838071f190cd17eb777d
jeeson12 <jeeson7012@gmail.com>
2025-04-13 00:58:42 +0530

    width change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {assets} from '../assets/assets'
 const navbar = () => {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      <img src={assets.logo} className="w-36" alt=''/>
+      <img src={assets.logo} className="w-12" alt=''/>
     </div>
   )
 }
